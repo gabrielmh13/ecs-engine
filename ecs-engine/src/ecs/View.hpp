93dc4d1d6e0f0d3dd2ad4bfbd189fe35ab9433d7
Gabriel Hernandes <gabrielmh13@hotmail.com>
2026-01-08 23:16:10 -0300

    feat: add smaller pool selection optimization

--- ecs-engine/src/ecs/View.hpp
+++ ecs-engine/src/ecs/View.hpp
@@ -20,11 +20,52 @@ namespace Engine::ECS {
 
 		template<typename Func>
 		void Each(Func func) {
-			auto& basePool = std::get<0>(m_Pools);
+			std::size_t smallestPoolIndex = FindSmallestPoolIndex();
+			IterateSmallestPool(func, smallestPoolIndex, std::index_sequence_for<Components...>{});
+		}
+
+	private:
+		Registry& m_Registry;
+		std::tuple<ComponentPool<Components>&...> m_Pools;
+
+		std::size_t FindSmallestPoolIndex() const {
+			return FindSmallestPoolIndexHelper(std::index_sequence_for<Components...>{});
+		}
+
+		template<std::size_t... I>
+		std::size_t FindSmallestPoolIndexHelper(std::index_sequence<I...>) const {
+			std::size_t sizes[] = { std::get<I>(m_Pools).data.size()... };
+			std::size_t minIndex = 0;
+			std::size_t minSize = sizes[0];
+
+			for (std::size_t i = 1; i < sizeof...(Components); ++i) {
+				if(sizes[i] < minSize) {
+					minSize = sizes[i];
+					minIndex = i;
+				}
+			}
+
+			return minIndex;
+		}
+
+		template<typename Func, std::size_t... I>
+		void IterateSmallestPool(Func& func, std::size_t poolIndex, std::index_sequence<I...>) {
+			(IterateIfMatch<I>(func, poolIndex), ...);
+		}
+
+		template<std::size_t PoolIndex, typename Func>
+		void IterateIfMatch(Func& func, std::size_t index) {
+			if (index == PoolIndex) {
+				IteratePool<PoolIndex>(func, std::index_sequence_for<Components...>{});
+			}
+		}
+
+		template<std::size_t PoolIndex, typename Func, std::size_t... I>
+		void IteratePool(Func& func, std::index_sequence<I...>) {
+			auto& basePool = std::get<PoolIndex>(m_Pools);
 
 			for (auto& [entityID, _] : basePool.data) {
-
-				if (!HasAllComponents(entityID))
+				if(!HasAllComponents<PoolIndex>(entityID, std::index_sequence<I...>{}))
 					continue;
 
 				Entity e{
@@ -32,22 +73,16 @@ namespace Engine::ECS {
 					m_Registry.GetGeneration(entityID)
 				};
 
-				Invoke(func, entityID, std::index_sequence_for<Components...>{}, e);
+				Invoke(func, entityID, std::index_sequence<I...>{}, e);
 			}
 		}
 
-	private:
-		Registry& m_Registry;
-		std::tuple<ComponentPool<Components>&...> m_Pools;
 
-		template<std::size_t... I>
+		template<std::size_t SkipIndex, std::size_t... I>
 		bool HasAllComponents(uint32_t entityID, std::index_sequence<I...>) {
-			return (... && std::get<I>(m_Pools).data.contains(entityID));
+			return (... && (I == SkipIndex || std::get<I>(m_Pools).data.contains(entityID)));
 		}
-
-		bool HasAllComponents(uint32_t entityID) {
-			return HasAllComponents(entityID, std::index_sequence_for<Components...>{});
-		}
+		
 
 		template<typename Func, std::size_t... I>
 		void Invoke(Func& func, uint32_t entityID, std::index_sequence<I...>, Entity& e) {
